2afbcefc1782e2b536c1b4ba5bcd12c17f8479f1
Julio <>
2017-09-28 12:26:10 -0600

    Change to Class Component

--- index.ios.js
+++ index.ios.js
@@ -1,12 +1,16 @@
 
 // Import a Library to help create component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
+  <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 
