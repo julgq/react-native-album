bacf99f13ca74fe4c4218fd1384eaa7bb07118de
Julio <>
2017-09-29 19:53:26 -0600

    Using CardSection flex:1

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,21 +4,34 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    headerTextStyle,
+    thumbnailContainerStyle,
+    imageStyle,
+  } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
-          style={styles.thumbnailStyle}
+          style={thumbnailStyle}
           source={{ uri: thumbnail_image }}
           />
         </View>
-        <View style={styles.headerContentStyle}>
-          <Text>{title}</Text>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={imageStyle}
+        />
       </CardSection>
     </Card>
   );
@@ -29,10 +42,24 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around',
   },
+  headerTextStyle: {
+    fontSize: 18,
+  },
   thumbnailStyle: {
     height: 50,
     width: 50,
   },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    width: null,
+    flex: 1,
+  }
 };
 
 export default AlbumDetail;
