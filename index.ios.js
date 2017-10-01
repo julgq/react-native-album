c308c7eedaac865c76bd6e32f45993877cb5b448
Julio <>
2017-09-30 20:34:41 -0600

    Button

--- index.ios.js
+++ index.ios.js
@@ -5,14 +5,20 @@ import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
+
 // Create a component
 const App = () => (
-  <View>
+  <View style={styles.ScrollFlex}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>
 );
 
+const styles = {
+  ScrollFlex: {
+    flex: 1,
+  },
+};
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
